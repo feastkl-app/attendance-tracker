0040d0e781d794de394092d5f642dffad9549087
Randolph Pe Benito <rpebenito@xyber.ph>
2018-02-01 00:55:24 +0800

    Add login and logout functionality. Chore: fix login design.

--- webapp/event_attendance/views.py
+++ webapp/event_attendance/views.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'home.html', {})
 
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
