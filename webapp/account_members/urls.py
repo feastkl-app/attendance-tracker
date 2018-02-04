9754dcbbedd071c9a1d8df395fe9581992d73805
Randolph Pe Benito <rpebenito@xyber.ph>
2018-02-05 00:15:07 +0800

    Fix breadcrumbs for member add. Add member list page with styling. Fix navbar.

--- webapp/account_members/urls.py
+++ webapp/account_members/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 
 
 from .views import (
-    MemberProfileCreateView
+    MemberProfileCreateView,
+    MemberProfileListView,
 )
+
 urlpatterns = [
     # Member
+    url(r'^$',  MemberProfileListView.as_view(), name='member-profile-list'),
     url(r'^add/$',  MemberProfileCreateView.as_view(), name='member-profile-add'),
 ]
--- webapp/account_members/views.py
+++ webapp/account_members/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from .forms import MemberProfileForm 
 from .models import MemberProfile
 
-# Create your views here.
 class MemberProfileCreateView(CreateView):
     model = MemberProfile
     form_class = MemberProfileForm
     template_name = 'account_members/member_profile_add_form.html'
     success_url = reverse_lazy('home')
+
+class MemberProfileListView(ListView):
+    model = MemberProfile
+    context_object_name = 'member'
+    template_name = 'account_members/member_list.html'
 
