104eac506a0ef0a0cf78fa490a055506679b15e9
Randolph Pe Benito <rpebenito@xyber.ph>
2018-08-27 00:05:01 +0800

    Chore: create fixture for testing events.

--- webapp/events/tests.py
+++ webapp/events/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+from attendance.models import EventAttendance
+from .models import Event
+
+
 # Create your tests here.
+class CreateEventTests(TestCase):
+    fixtures = ['events']
+    def setUp(self):
+        # Create 
+        pass
+
+    def test_create_event_generate_attendance(self):
+        pass
+        #event = Event.object.get(id=1)
+        #print(event)
+        #print('testing')
